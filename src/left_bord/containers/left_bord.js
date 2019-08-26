47ff1232f244513f007ac066c1f9475d48abf128
Mortem45 <ste45_007@hotmail.com>
2019-08-25 23:14:16 -0600

    social wrap ready

--- src/left_bord/containers/left_bord.js
+++ src/left_bord/containers/left_bord.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import LeftBord from '../components/left_bord'
 import MenuBtn from '../components/menu_btn'
+import SocialWrap from '../components/social_wrap'
 
 export default class LeftBoardContainer extends Component {
   render() {
     return (
       <LeftBord>
-        <MenuBtn/>
+        <MenuBtn />
+        <SocialWrap />
       </LeftBord>
     )
   }
